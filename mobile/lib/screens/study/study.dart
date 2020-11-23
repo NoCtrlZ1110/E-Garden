712aca3e6edacb3e8dd853093fdab36128efcc70
manhdvuetk63 <57102439+manhdvuetk63@users.noreply.github.com>
2020-11-23 19:33:42 +0700

    add api dictionary

--- mobile/lib/screens/study/study.dart
+++ mobile/lib/screens/study/study.dart
@@ -6,7 +6,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 
-import 'exam.dart';
 import 'learn.dart';
 
 class StudyScreen extends StatefulWidget {
