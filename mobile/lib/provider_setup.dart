cd6b473c5840ec871627f65de89bec506cf121dc
manhdvuetk63 <natrihoatri1@gmail.com>
2020-11-24 18:05:51 +0700

    Screen Dictionary

--- mobile/lib/provider_setup.dart
+++ mobile/lib/provider_setup.dart
@@ -1,11 +1,15 @@
+import 'package:e_garden/core/services/dictionary/dictionary_model.service.dart';
+import 'package:e_garden/core/services/translate/translate_model.service.dart';
 import 'package:provider/provider.dart';
 import 'package:provider/single_child_widget.dart';
-import 'package:e_garden/core/view_model/dictionary_model.dart';
 
 List<SingleChildWidget> getProviders() {
   var independentServices = [
     ChangeNotifierProvider(
       create: (context) => DictionaryModel(),
+    ),
+    ChangeNotifierProvider(
+      create: (context) => TranslateModel(),
     ),
   ];
 
