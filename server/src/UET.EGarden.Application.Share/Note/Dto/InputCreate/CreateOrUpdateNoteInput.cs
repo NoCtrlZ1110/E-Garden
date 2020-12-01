f9a8ee6f634f33d12c4ae8ee908db0a92336bc71
manhdvuetk63 <natrihoatri1@gmail.com>
2020-12-01 16:18:26 +0700

    API Note screen

---
+++ server/src/UET.EGarden.Application.Share/Note/Dto/InputCreate/CreateOrUpdateNoteInput.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Abp.Application.Services.Dto;
+
+namespace UET.EGarden.Note.Dto.InputCreate
+{
+    public class CreateOrUpdateNoteInput : EntityDto<long>
+    {
+        public long UserId { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+        public DateTime Date { get; set; }
+        public string TitleNote { get; set; }
+        public string DetailNote { get; set; }
+        public bool Status { get; set; }
+    }
+}
