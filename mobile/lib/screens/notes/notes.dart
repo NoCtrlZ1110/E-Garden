ef21e21aaa06f3f08990981f9f499a7599927174
manhdvuetk63 <natrihoatri1@gmail.com>
2020-12-04 22:18:03 +0700

    add api get list note

--- mobile/lib/screens/notes/notes.dart
+++ mobile/lib/screens/notes/notes.dart
@@ -1,10 +1,11 @@
 import 'package:e_garden/configs/AppConfig.dart';
+import 'package:e_garden/core/models/notes/notes.dart';
 import 'package:e_garden/core/services/note/note_model.service.dart';
 import 'package:e_garden/screens/notes/component/task_container.dart';
-import 'package:e_garden/widgets/custom_app_bar.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:e_garden/utils/light_color.dart';
+import 'package:getwidget/getwidget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
 import 'package:provider/provider.dart';
 import 'package:table_calendar/table_calendar.dart';
 
@@ -26,9 +27,10 @@ class _CalendarPageState extends State<CalendarPage> {
             width: SizeConfig.screenWidth,
             height: SizeConfig.screenHeight,
             decoration: BoxDecoration(
-             // image: DecorationImage(image: AssetImage("assets/images/login/bg_screen.png"), fit: BoxFit.fill),
-              borderRadius:
-                  BorderRadius.only(topLeft: const Radius.circular(20.0), topRight: const Radius.circular(20.0)),
+              // image: DecorationImage(image: AssetImage("assets/images/login/bg_screen.png"), fit: BoxFit.fill),
+              borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(20.0),
+                  topRight: const Radius.circular(20.0)),
             ),
           ),
           Column(
@@ -40,7 +42,11 @@ class _CalendarPageState extends State<CalendarPage> {
                     return FutureBuilder(
                         future: notes.fetchListNote(params),
                         builder: (context, snapshot) {
-                          return (snapshot.hasData) ? Container(child: Text("Load"),) : Center(child: CircularProgressIndicator());
+                          return (snapshot.hasData)
+                              // ? _buildListNote(notes.listNote)
+                              ? _buildList(notes.listNote)
+                              //? note()
+                              : Center(child: CircularProgressIndicator());
                         });
                   },
                 ),
@@ -52,6 +58,23 @@ class _CalendarPageState extends State<CalendarPage> {
     );
   }
 
+
+  Widget _buildList(Notes notes) {
+    return StaggeredGridView.countBuilder(
+      crossAxisCount: 2,
+      itemCount: notes.items.length,
+      itemBuilder: (BuildContext context, int index) => Center(
+        child: TaskContainer(
+            isDone: notes.items.elementAt(index).status,
+            title: notes.items.elementAt(index).titleNote,
+            subtitle: notes.items.elementAt(index).detailNote,
+            num: index),
+      ),
+      staggeredTileBuilder: (int index) => StaggeredTile.fit(1),
+      mainAxisSpacing: 4.0,
+      crossAxisSpacing: 4.0,
+    );
+  }
   Widget _buildTableCalendar() {
     return TableCalendar(
       calendarController: _calendarController,
@@ -65,7 +88,8 @@ class _CalendarPageState extends State<CalendarPage> {
         outsideDaysVisible: false,
       ),
       headerStyle: HeaderStyle(
-        formatButtonTextStyle: TextStyle().copyWith(color: Colors.white, fontSize: 15.0),
+        formatButtonTextStyle:
+            TextStyle().copyWith(color: Colors.white, fontSize: 15.0),
         formatButtonDecoration: BoxDecoration(
           color: Colors.blue,
           borderRadius: BorderRadius.circular(16.0),
